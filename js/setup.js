e6491169c8864b3e39b0ccaf62f94405f3f2a9e3
asabantsev <asabantsev@gmail.com>
2019-06-30 14:07:27 +0300

    Добавлено открытие и закрытие окна, валидация персонажа и изменение его цвета

--- js/setup.js
+++ js/setup.js
@@ -4,21 +4,29 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+// находим элементы окна
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarListElement = setup.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-
 var randomInteger = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var randomArrayElement = function (array) {
+  return array[Math.floor(Math.random() * (array.length - 1))];
 };
 
 var randomObject = function (name, surname, coatColor, eyesColor) {
@@ -60,4 +68,85 @@ for (var i = 0; i < wizards.length; i++) {
 }
 similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+setup.querySelector('.setup-similar').classList.remove('hidden');
+
+// функции скрытия и раскрытия окна
+var onPopupEcsPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEcsPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEcsPress);
+};
+
+// обработчики событий
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+// валидация поля ввода имени
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+// изменение цвета по нажатию
+var wizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+var wizardCoatInput = setup.querySelector('input[name=coat-color]');
+
+var wizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
+var wizardEyesInput = setup.querySelector('input[name=eyes-color]');
+
+var fireBall = setup.querySelector('.setup-fireball-wrap');
+var fireBallInput = setup.querySelector('input[name=fireball-color]');
+
+var setWizardRandomColor = function (evt, colors, styleProperty, formInputNode) {
+  var color = randomArrayElement(colors);
+  evt.currentTarget.style[styleProperty] = color;
+  formInputNode.value = color;
+};
+
+wizardCoat.addEventListener('click', function (evt) {
+  setWizardRandomColor(evt, COAT_COLORS, 'fill', wizardCoatInput);
+});
+
+wizardEyes.addEventListener('click', function (evt) {
+  setWizardRandomColor(evt, EYES_COLORS, 'fill', wizardEyesInput);
+});
+
+fireBall.addEventListener('click', function (evt) {
+  setWizardRandomColor(evt, FIREBALL_COLORS, 'background', fireBallInput);
+});
